eb337990f4a69e25250e67f2d7b2dde8d9cd20bd
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-12 10:48:32 -0600

    added boiler plate for the useContext provider.

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, createContext, useContext, useReducer } from "react";
+import { createContext, type ReactNode } from "react";
 
 type Timer = {
   name: string;
@@ -10,47 +10,27 @@ type TimersState = {
   timers: Timer[];
 };
 
-const initialState: TimersState = {
-  isRunning: true,
-  timers: [],
-};
-
 type TimersContextValue = TimersState & {
   addTimer: (timerData: Timer) => void;
-  startTimers: () => void;
+  startTimer: () => void;
   stopTimers: () => void;
 };
 
 const TimersContext = createContext<TimersContextValue | null>(null);
 
-export function useTimersContext() {
-  const timersCtx = useContext(TimersContext);
-
-  if (timersCtx === null) {
-    throw new Error("TimersContext is null - that should not be the case.");
-  }
-
-  return timersCtx;
-}
-
 type TimersContextProviderProps = {
   children: ReactNode;
 };
 
-export default function TimersContextProvider({
-  children,
-}: TimersContextProviderProps) {
-  const reducer = useReducer(reducer, initialState);
-
+function TimersContextProvider({ children }: TimersContextProviderProps) {
   const ctx: TimersContextValue = {
     timers: [],
     isRunning: false,
     addTimer(timerData) {},
-    startTimers() {},
+    startTimer() {},
     stopTimers() {},
   };
   return (
     <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
   );
 }
-//
